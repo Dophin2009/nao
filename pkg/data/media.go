35e2fdd08755185356cd13c0a4d7b06ab05c552c
Eric Zhao <21zhaoe@protonmail.com>
2019-10-10 09:15:38 -0400

    Add when Media premiered

--- pkg/data/media.go
+++ pkg/data/media.go
@@ -9,15 +9,48 @@ import (
 
 // Media represents a single instance of a media
 type Media struct {
-	ID         int
-	Titles     []Info
-	Synopses   []Info
-	Background []Info
-	StartDate  *time.Time
-	EndDate    *time.Time
-	Type       string
-	Source     string
+	ID              int
+	Titles          []Info
+	Synopses        []Info
+	Background      []Info
+	StartDate       *time.Time
+	EndDate         *time.Time
+	SeasonPremiered Season
+	Type            string
+	Source          string
 }
+
+// Season contains information about the quarter
+// and year
+type Season struct {
+	Quarter Quarter
+	Year    int
+}
+
+// Quarter represents the quarter of the year
+// by integer
+type Quarter int
+
+const (
+	// Winter is the first quarter of the year,
+	// encapsulating the months January, February,
+	// and March
+	Winter Quarter = iota
+
+	// Spring is the second quarter of the year,
+	// encapsulating the months April, May, and June
+	Spring
+
+	// Summer is the third quarter of the year,
+	// encapsulating the months July, August, and
+	// September
+	Summer
+
+	// Fall is the fouth quarter of the year,
+	// encapsulating the months October,
+	// November, and December
+	Fall
+)
 
 const mediaBucketName = "Media"
 

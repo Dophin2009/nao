9d8e8001eaa9a075d392f8dca295e4e4d1e4958f
Eric Zhao <21zhaoe@protonmail.com>
2019-10-09 22:46:43 -0400

    Add function to get episodes by Media

--- pkg/data/episode.go
+++ pkg/data/episode.go
@@ -71,6 +71,34 @@ func EpisodeGet(ID int, db *bolt.DB) (ep Episode, err error) {
 	return
 }
 
+// EpisodeGetByMedia retrieves a list of instances of Episode
+// with the given Media ID
+func EpisodeGetByMedia(mID int, db *bolt.DB) (list []Episode, err error) {
+	err = db.View(func(tx *bolt.Tx) error {
+		// Get Episode bucket, exit if error
+		b, err := bucket(episodeBucketName, tx)
+		if err != nil {
+			return err
+		}
+
+		// Get Episode by Media ID
+		return b.ForEach(func(k, v []byte) (err error) {
+			ep := Episode{}
+			err = json.Unmarshal(v, &ep)
+			if err != nil {
+				return err
+			}
+
+			if ep.MediaID == mID {
+				list = append(list, ep)
+			}
+			return nil
+		})
+	})
+
+	return
+}
+
 // EpisodeCreate persists a new instance of Episode
 func EpisodeCreate(ep *Episode, db *bolt.DB) error {
 	return db.Update(func(tx *bolt.Tx) error {

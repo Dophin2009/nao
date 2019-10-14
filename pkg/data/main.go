d889f4a4d31b8726e29f5fb22c3b4f1ba24c9058
Eric Zhao <21zhaoe@protonmail.com>
2019-10-14 19:43:59 -0400

    Create generic persistence layer functions

--- pkg/data/main.go
+++ pkg/data/main.go
@@ -1,6 +1,10 @@
 package data
 
 import (
+	"encoding/binary"
+	"encoding/json"
+	"fmt"
+
 	bolt "go.etcd.io/bbolt"
 )
 
@@ -45,4 +49,151 @@ func ClearDatabase(db *bolt.DB) (err error) {
 	})
 
 	return
+}
+
+// Idenitifiable encompasses all the entities
+// persisted in the database
+type Idenitifiable interface {
+	Identifier() int
+	SetIdentifier(int)
+
+	Ver() int
+	SetVer(int)
+}
+
+// getByID is a generic function that queries the given bucket
+// in the given database for an entity of the given ID
+func getByID(ID int, entity Idenitifiable, bucketName string, db *bolt.DB) (err error) {
+	return db.View(func(tx *bolt.Tx) error {
+		// Get bucket, exit if error
+		b, err := bucket(bucketName, tx)
+		if err != nil {
+			return err
+		}
+
+		// Get entity by ID, exit if error
+		v, err := get(ID, b)
+		if err != nil {
+			return err
+		}
+
+		return json.Unmarshal(v, entity)
+	})
+}
+
+func getFilter(t Idenitifiable, filter func(e Idenitifiable) (bool, error), bucketName string, db *bolt.DB) (list []Idenitifiable, err error) {
+	err = db.View(func(tx *bolt.Tx) error {
+		// Get bucket, exit if error
+		b, err := bucket(bucketName, tx)
+		if err != nil {
+			return err
+		}
+
+		// Unmarshal and add all entities who
+		// pass filter to slice, exit if error
+		return b.ForEach(func(k, v []byte) error {
+			m := t
+			err = json.Unmarshal(v, &m)
+			if err != nil {
+				return err
+			}
+
+			pass, err := filter(m)
+			if err != nil {
+				return err
+			}
+			if pass {
+				list = append(list, m)
+			}
+			return nil
+		})
+	})
+	return
+}
+
+// create is a generic function that persists an
+// entity into the given bucket in the given
+// databases
+func create(entity Idenitifiable, bucketName string, db *bolt.DB) (err error) {
+	return db.Update(func(tx *bolt.Tx) error {
+		// Get bucket, exit if error
+		b, err := bucket(bucketName, tx)
+		if err != nil {
+			return err
+		}
+
+		// Get next ID in sequence and
+		// assign to entity
+		id, err := b.NextSequence()
+		if err != nil {
+			return err
+		}
+		entity.SetIdentifier(int(id))
+		entity.SetVer(1)
+
+		// Save entity in bucket
+		buf, err := json.Marshal(entity)
+		if err != nil {
+			return err
+		}
+
+		return b.Put(itob(entity.Identifier()), buf)
+	})
+}
+
+// update is a generic function replaces the value
+// of the given entity id in the given bucket
+// in the given database
+func update(entity Idenitifiable, bucketName string, db *bolt.DB) (err error) {
+	return db.Update(func(tx *bolt.Tx) error {
+		// Get bucket, exit if error
+		b, err := bucket(bucketName, tx)
+		if err != nil {
+			return err
+		}
+
+		// Check if entity with ID exists
+		o, err := get(entity.Identifier(), b)
+		if err != nil {
+			return err
+		}
+
+		// Replace properties of new with internal
+		// ones of old
+		old := entity
+		err = json.Unmarshal([]byte(o), &old)
+		// Update version
+		entity.SetVer(old.Ver() + 1)
+
+		// Save Media
+		buf, err := json.Marshal(entity)
+		if err != nil {
+			return err
+		}
+
+		return b.Put(itob(entity.Identifier()), buf)
+	})
+}
+
+func bucket(name string, tx *bolt.Tx) (bucket *bolt.Bucket, err error) {
+	bucket = tx.Bucket([]byte(name))
+	return
+}
+
+func get(ID int, bucket *bolt.Bucket) (v []byte, err error) {
+	if bucket == nil {
+		return nil, fmt.Errorf("bucket must not be nil")
+	}
+
+	v = bucket.Get(itob(ID))
+	if v == nil {
+		return nil, fmt.Errorf("entity with id %d not found", ID)
+	}
+	return v, nil
+}
+
+func itob(v int) []byte {
+	b := make([]byte, 8)
+	binary.BigEndian.PutUint64(b, uint64(v))
+	return b
 }
